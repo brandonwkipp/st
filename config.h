25e6aaf99df49f6e27271d89a6eff0992d430c57
brandonwkipp <brandonwkipp@gmail.com>
2024-03-24 23:06:07 -0500

    updated bkg color

--- config.h
+++ config.h
@@ -124,7 +124,7 @@ static const char *colorname[] = {
 	"#cccccc",
 	"#555555",
 	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#1E1E3F", // "black", /* default background colour */
 };
 
 
